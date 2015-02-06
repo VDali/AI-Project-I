3392e7e79764642bb117f67adbbfc6870612fb55
Veena Dali <VeenaDali@gmail.com>
2015-02-05 19:34:21 -0500

    Initial Commit

---
+++ AI Project I/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AI Project I
+//
+//  Created by Veena Dali on 2/5/15.
+//  Copyright (c) 2015 Veena Dali. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
